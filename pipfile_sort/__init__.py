bb0c1d85ca9ceff9b162439d5026ce56dc43c752
Sho Minagawa <msh5.global@gmail.com>
2021-08-31 21:32:52 +0900

    Bump app version to 0.2.0 (#18)

--- pipfile_sort/__init__.py
+++ pipfile_sort/__init__.py
@@ -5,7 +5,7 @@ from plette import Pipfile
 from plette.pipfiles import PackageCollection
 import sys
 
-APP_VERSION = '0.1.0'
+APP_VERSION = '0.2.0'
 PIPFILE_FILENAME = './Pipfile'
 PIPFILE_ENCODING = 'utf-8'
 
